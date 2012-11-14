ecacfc94320646e0ef333f0d972cb85854c88d46
zhaohaifeng <zhaohaifeng@meituan.com>
2012-11-14 23:51:50 +0800

    next page

--- src/com/impulse/study/helloworld/MainActivity.java
+++ src/com/impulse/study/helloworld/MainActivity.java
@@ -2,19 +2,34 @@ package com.impulse.study.helloworld;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);// ÃÓ≥‰±ÍÃ‚¿∏
+		setContentView(R.layout.activity_main);
+		Button nextPage = (Button) findViewById(R.id.button1);
+		nextPage.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent showNextPage_Intent = new Intent();
+				showNextPage_Intent.setClass(MainActivity.this,
+						NextpageActivity.class);
+				startActivity(showNextPage_Intent);
+			}
+		});
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
 }
